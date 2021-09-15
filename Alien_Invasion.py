53ef076961eb9fedfaca5cbe709a14534e794f22
Omar El Gendy <omarhatemelgendy@gmail.com>
2021-09-16 01:50:04 +0200

    Building the program

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from settings import Settings
-from pygame.constants import QUIT
+from ship import Ship
 
 def run_game():
     #Initializing the game window and setting the caption
@@ -9,8 +9,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    #setting the background color of the screen
-    bg_color = (230, 230, 230)
+    ship = Ship(screen)
     #Running the game main loop
     while True:
         #Evaluating Events by mouse and keyboard
@@ -19,6 +18,8 @@ def run_game():
                 sys.exit()
         #Redrawing the color every loop
         screen.fill(ai_settings.bg_color)
+        #Drawing the ship
+        ship.blitme()
         #Making the last drawn screen visible
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -4,6 +4,5 @@ class Settings():
         """Initialize the game settings"""
         #Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
-        
